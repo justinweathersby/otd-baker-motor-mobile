2951fa9ac77ec6b88ebf6e7e362a4735ab0f17a4
justinweathersby <justinweathersby@gmail.com>
2016-10-12 11:00:53 -0400

    Setup app for dev push notifications

--- www/js/app.js
+++ www/js/app.js
@@ -9,12 +9,21 @@ var app = angular.module('otd-baker-motor-app', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
+
+    var push = new Ionic.Push({
+      "debug": true
+    });
+
+    push.register(function(token){
+      console.log("My Device Token: ", token.token);
+      push.saveToken(token); // persist the token in the Ionic Platform
+    });
+
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
-
     }
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
